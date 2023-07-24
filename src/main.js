7cc5ffcbab6b5bda6f7bc58d7e198a3b92c459ed
Dolphinchair <hatten.lois@outlook.fr>
2023-07-24 14:18:43 +0200

    feat: add descritpion

--- src/main.js
+++ src/main.js
@@ -2,79 +2,79 @@ let tableaux=
 [
     {
         "id_tab":"1",
-        "name_tab":"asiatique",
-        "dim_tab":"PLACEHOLDER",
-        "date_tab":"PLACEHOLDER",
+        "name_tab":"Migration",
+        "dim_tab":"61x50",
+        "date_tab":"Hule sur toile",
         "tab_img":"../tableaux/1.jpg"
     },
     {
         "id_tab":"2",
-        "name_tab":"rouge",
-        "dim_tab":"PLACEHOLDER",
-        "date_tab":"PLACEHOLDER",
+        "name_tab":"Maître tibétain",
+        "dim_tab":"50x61",
+        "date_tab":"Huile sur toile",
         "tab_img":"../tableaux/1bis.jpg"
     },
     {
         "id_tab":"3",
-        "name_tab":"automne",
-        "dim_tab":"PLACEHOLDER",
-        "date_tab":"PLACEHOLDER",
+        "name_tab":"Automne",
+        "dim_tab":"61x50",
+        "date_tab":"Huile sur toile",
         "tab_img":"../tableaux/2.jpg"
     },
     {
         "id_tab":"4",
-        "name_tab":"printemps",
-        "dim_tab":"PLACEHOLDER",
-        "date_tab":"PLACEHOLDER",
+        "name_tab":"Printemps",
+        "dim_tab":"61x50",
+        "date_tab":"Huile sur toile",
         "tab_img":"../tableaux/3.jpg"
     },
     {
         "id_tab":"5",
-        "name_tab":"hiver",
-        "dim_tab":"PLACEHOLDER",
-        "date_tab":"PLACEHOLDER",
+        "name_tab":"Hiver",
+        "dim_tab":"61x50",
+        "date_tab":"Huile sur toile",
         "tab_img":"../tableaux/4.jpg"
     },
     {
         "id_tab":"6",
-        "name_tab":"été",
-        "dim_tab":"PLACEHOLDER",
-        "date_tab":"PLACEHOLDER",
+        "name_tab":"Été",
+        "dim_tab":"61x50",
+        "date_tab":"Huile sur toile",
         "tab_img":"../tableaux/5.jpg"
     },
     {
         "id_tab":"7",
-        "name_tab":"dieufemme",
-        "dim_tab":"PLACEHOLDER",
-        "date_tab":"PLACEHOLDER",
+        "name_tab":"Dieu créa la femme",
+        "dim_tab":"60x81",
+        "date_tab":"Huile sur toile",
         "tab_img":"../tableaux/6.jpg"
     },
     {
         "id_tab":"8",
-        "name_tab":"createur",
-        "dim_tab":"PLACEHOLDER",
-        "date_tab":"PLACEHOLDER",
+        "name_tab":"Le créateur",
+        "dim_tab":"93x67",
+        "date_tab":"Huile sur bois",
         "tab_img":"../tableaux/7.jpg"
     },
     {
         "id_tab":"9",
-        "name_tab":"dieuhomme",
-        "dim_tab":"PLACEHOLDER",
-        "date_tab":"PLACEHOLDER",
+        "name_tab":"Dieu créa l'homme",
+        "dim_tab":"60x81",
+        "date_tab":"Huile sur toile",
         "tab_img":"../tableaux/8.jpg"
     },
     {
         "id_tab":"10",
-        "name_tab":"soumission",
-        "dim_tab":"PLACEHOLDER",
-        "date_tab":"PLACEHOLDER",
+        "name_tab":"La soumission",
+        "dim_tab":"41x33",
+        "date_tab":"Huile sur toile",
         "tab_img":"../tableaux/9.jpg"
     },
     {
         "id_tab":"11",
-        "name_tab":"cheval",
-        "dim_tab":"PLACEHOLDER",
-        "date_tab":"PLACEHOLDER",
+        "name_tab":"Tête de cheval à la crinière d'or",
+        "dim_tab":"49x54",
+        "date_tab":"Huile sur bois",
         "tab_img":"../tableaux/10.jpg"
     }
 ];
